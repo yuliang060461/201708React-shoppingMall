fd1c6db16a2150f015735211097530326f0f6b71
yuliang <yuliang060461@163.com>
2017-11-20 12:59:29 +0800

    后台4路由搭建完毕

--- mock/index.js
+++ mock/index.js
@@ -1,26 +1,20 @@
 let express = require('express');
-let fs=require("fs")
+let fs=require("fs");
 let app = express();
-
-
-
-app.get("/globalselect",function (req,res) {
-    res.set('Content-Type','text/plain');
-   fs.createReadStream("./globalselect/sliders.json").pipe(res)
-})
-
-app.listen(8080);
-
-
-// function read(cb) {
-//     fs.readFile('./globalselect/sliders.json','utf8',function (err,data) {
-//         if(err || data.length==0){ //如果文件不存在或者内容是空 传递空数组
-//             cb([]);
-//         }else{
-//             cb(JSON.parse(data));
-//         }
-//     })
-// }
-// function write(data,cb) {
-//     fs.writeFile('./globalselect/sliders.json',JSON.stringify(data),cb);
-// }
+app.get("/globalSelect",function (req,res) {
+    res.set('Content-Type','application/json');
+   fs.createReadStream("./globalSelect/globalSelect.json").pipe(res)
+});
+app.get("/multipointSupermarket",function (req,res) {
+    res.set('Content-Type','application/json');
+    fs.createReadStream("./multipointSupermarket/MultipointSupermarket.json").pipe(res)
+});
+app.get("/hotSale",function (req,res) {
+    res.set('Content-Type','application/json');
+    fs.createReadStream("./hotSale/hotSale.json").pipe(res)
+});
+app.get("/worthBuy",function (req,res) {
+    res.set('Content-Type','application/json');
+    fs.createReadStream("./worthBuy/worthBuy.json").pipe(res)
+});
+app.listen(9000);
