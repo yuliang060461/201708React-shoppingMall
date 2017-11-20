7a236ecf0e5e5f03b2ddbb1b603bc97f1e620165
yuliang <yuliang060461@163.com>
2017-11-20 11:57:12 +0800

    删idea 创建createReadStrem

--- mock/index.js
+++ mock/index.js
@@ -1,6 +1,26 @@
-let http=require("http");
-let url=require("url");
+let express = require('express');
+let fs=require("fs")
+let app = express();
 
-http.listen(3000,function () {
-    console.log(" 3000端口监听成功");
+
+
+app.get("/globalselect",function (req,res) {
+    res.set('Content-Type','text/plain');
+   fs.createReadStream("./globalselect/sliders.json").pipe(res)
 })
+
+app.listen(8080);
+
+
+// function read(cb) {
+//     fs.readFile('./globalselect/sliders.json','utf8',function (err,data) {
+//         if(err || data.length==0){ //如果文件不存在或者内容是空 传递空数组
+//             cb([]);
+//         }else{
+//             cb(JSON.parse(data));
+//         }
+//     })
+// }
+// function write(data,cb) {
+//     fs.writeFile('./globalselect/sliders.json',JSON.stringify(data),cb);
+// }
