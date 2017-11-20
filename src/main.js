3a42d624d57b34eaa1b8bc8ffdcead6ea940bac7
zhixingheyi2017 <823169696@qq.com>
2017-11-21 02:42:10 +0800

    1711210242

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Vip from './container/index'
 
 class Content extends Component{
     render(){
         return(<div>
-            脚手架搭建完毕
+            <Vip/>
         </div>)
     }
 }
