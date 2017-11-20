40290d3caf2e2d443ddd6333add2d8e52dcbab43
yuliang <yuliang060461@163.com>
2017-11-20 19:49:33 +0800

    2.0

--- mock/index.js
+++ mock/index.js
@@ -13,6 +13,11 @@ app.use(function(req,res,next){
     //允许客户端发送Cookie
     res.header('Access-Control-Allow-Credentials',"true");
     //当客户端发向服务器发post跨域的时候，会先发送OPTIONS请求。如果服务器返回的响应头Access-Control-Allow-Methods里有POST的话，才会再次发送POST请求
+    if (req.method == 'OPTIONS') {
+        res.end('');
+    } else {
+        next();
+    }
 });
 
 
