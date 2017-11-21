f985256760e1074c5f40332e58635f60710ec6a6
yuliang <yuliang060461@163.com>
2017-11-21 13:53:09 +0800

    2.0

---
+++ mock/userList.js
@@ -0,0 +1,5 @@
+module.exports = [
+    {"username": "higo", "password": "higo", "cartList": []},
+    { "username": "2","password": "3","cartList": []},
+    {"username": "3333", "password": "aaa", "cartList": []},
+    {"username": "111", "password": "111", "cartList": []}];
