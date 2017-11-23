1f68e5918627c96ce0dcf4dca88e911719537cbe
aralewby <cocodoor@sina.cn>
2017-11-23 10:35:13 +0800

    增加cart的reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -9,7 +9,7 @@ import {routerReducer} from 'react-router-redux';
 import search from './search'
 import home from './home'
 import session from './session'
-
+import cart from './cart'
 import vip from './vip'
 export default combineReducers({
     routerReducer,
