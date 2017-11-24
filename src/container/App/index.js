c8f836e2fffb926d86738b88b69f5e90ed77a984
zhixingheyi2017 <823169696@qq.com>
2017-11-24 18:31:42 +0800

    1124

--- src/container/App/index.js
+++ src/container/App/index.js
@@ -14,16 +14,13 @@ import ShoppingCart from '../ShoppingCart';
 import Login from "../Login/index";
 import Register from "../Register/index";
 import Vipcore from "../Vip/vip-core/index";
-<<<<<<< HEAD
 import Vipsign from "../Vip/vip-sign/index";
-=======
-import VipHomeLog from "../Vip/vip-home-log/index";
+import VipHomeLog from "../Vip/vip-home/index";
 import Order from "../Profile/Order/index";
 import ConfirmOrder from "../ConfirmOrder/index";
 import PlaceOrder from "../PlaceOrder/index";
 import Detail from '../Detail';
-import './index.less'
->>>>>>> feb6138c8bd5f7a99f8304852e56e04736654486
+import './index.less';
 export default class App extends Component{
     render(){
         return (
@@ -40,17 +37,12 @@ export default class App extends Component{
                     <Route path="/searchlist" component={SearchList}/>
                     <Route path="/vip" component={Vip}/>
                     <Route path="/vipcore" component={Vipcore}/>
-<<<<<<< HEAD
                     <Route path="/vipsign" component={Vipsign}/>
-
-
-=======
                     <Route path="/viphomelog" component={VipHomeLog}/>
                     <Route path="/order" component={Order}/>
                     <Route path="/confirmorder" component={ConfirmOrder}/>
                     <Route path="/placeorder" component={PlaceOrder}/>
                     <Route path="/detail" component={Detail}/>
->>>>>>> feb6138c8bd5f7a99f8304852e56e04736654486
                     <Tab/>
                 </div>
             </ConnectedRouter>
