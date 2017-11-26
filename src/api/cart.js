e95907d6ab09475d4e512b61c3a4d526256b698a
yingyzhao <1147279036@qq.com>
2017-11-26 08:14:37 +0800

    合并

--- src/api/cart.js
+++ src/api/cart.js
@@ -1,16 +1,7 @@
-import {get,post,del} from './index';
-export function getShop(username) {
-    return get('/getBus/'+username);
+import {get,post} from './index';
+export function getShop() {
+    return get('/getBus');
 }
-
-export function postOrderData(data,username) {
-    return post('/order/'+username,data)
-}
-
-export function postPlusData(data,username) {
-    return post('/writeBus/'+username,data)
-}
-
-export function delShop(data,username) {
-    return del('/deleteBus/'+username,data)
+export function postOrderData(data) {
+    return post('/',data)
 }
